af410981708aa833c63e39e665f2f35054273bc7
Carol Pinheiro <61391061+CarolPinheiro@users.noreply.github.com>
2022-12-09 21:59:09 +0000

    feat: added routes to exercise flask

--- Ex_Files/04_02_begin/main.py
+++ Ex_Files/04_02_begin/main.py
@@ -1,3 +1,16 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+@app.route("/second_route")
+def second_hello():
+    return "Second Hello World 1!"
+
+
+app.run(debug=True)
