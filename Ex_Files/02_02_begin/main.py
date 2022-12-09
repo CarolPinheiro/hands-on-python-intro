8302f5b7bc082a44d9005fa4d671155ae4a193c3
Carol Pinheiro <61391061+CarolPinheiro@users.noreply.github.com>
2022-12-09 13:33:40 +0000

    feat: added exercises of string formatted

--- Ex_Files/02_02_begin/main.py
+++ Ex_Files/02_02_begin/main.py
@@ -1,12 +1,18 @@
 greet = "Hello World"
 extened_grt = "Hello World, " + "this is a long string"
 
-name = "John"
+name = "Maria"
+
+name2 = "Julia"
 
 intrupution = f"Hello {name}"
 
 greet_format = "Hello {}"
 
-formatted = greet_format.format(name)
+formatted = greet_format.format(name2)
 
 print(intrupution, formatted)
+
+print(formatted.upper())
+print(formatted.lower())
+print(formatted.replace("Julia", "Cris"))
